a6187e57a36500389a99f74886c26348dccfc190
Ricardo <icorreo530@gmail.com>
2024-02-26 22:05:12 -0600

    new gittt
    nuevo gif

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -4,7 +4,7 @@ import {useState} from 'react';
 
 export const GifExpertApp = () => {
 
-  const [state, setstate] = useState([ 'ball' ]);
+  const [state, setstate] = useState([ 'git' ]);
   // Tarea
   const onAddCategory = (newCategory) => {
     if(state.includes(newCategory)) return;
